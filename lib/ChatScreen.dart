eb5203ec1446e105bda9b9856656734ed8ca683e
caiosousa21 <caiofmsousa@gmail.com>
2019-11-27 01:31:28 -0300

    Add user input to ChatScreen

--- lib/ChatScreen.dart
+++ lib/ChatScreen.dart
@@ -3,17 +3,55 @@ import 'EmailScreen.dart';
 
 class ChatScreen extends StatelessWidget {
   TextEditingController userInput = new TextEditingController();
+  List<ListTile> tiles = new List<ListTile>();
   Widget getListView() {
     var listView = ListView(
       children: <Widget>[
         ListTile(
-            leading: Icon(Icons.landscape),
-            title: Text("Plínio"),
-            subtitle: Text("Olá, essa é uma mensagem genérica"),
+          leading: Image.asset('Resources/bot.png', height: 40, width: 40),
+          title: Text("Plínio"),
+          subtitle: Text("Olá! Sou Plínio, seu assistente virtual!"),
         ),
       ],
     );
     return listView;
+  }
+
+  Widget _buildTileItem(BuildContext context, int index) {
+    if (tiles.length == 0){
+      return Card(
+          child: Column(
+          children: <Widget>[
+              ListTile(
+              leading: Image.asset('Resources/bot.png', height: 40, width: 40),
+            title: Text("Plínio"),
+            subtitle: Text("Olá! Sou Plínio, seu assistente virtual!"),)
+          ],
+        ),
+      );
+    }
+    else{
+      return Card(
+        child: Column(
+          children: <Widget>[
+            tiles.elementAt(index)
+          ],
+        ),
+      );
+    }
+  }
+
+  void setListTiles(String input) {
+    tiles.add(ListTile(
+        title: Text("Usuário"),
+        subtitle: Text(input),
+        trailing: Icon(Icons.wb_sunny)
+    ));
+    tiles.add(ListTile(
+      leading: Image.asset('Resources/bot.png', height: 40, width: 40),
+      title: Text("Plínio"),
+      subtitle: Text("Olá! Sou Plínio, seu assistente virtual!"),
+    ));
   }
 
   Widget getNewMessage(String input) {
@@ -23,11 +61,6 @@ class ChatScreen extends StatelessWidget {
             title: Text("Usuário"),
             subtitle: Text(input),
             trailing: Icon(Icons.wb_sunny)
-        ),
-        ListTile(
-          leading: Icon(Icons.landscape),
-          title: Text("Plínio"),
-          subtitle: Text("Olá, essa é uma mensagem genérica"),
         ),
       ],
     );
@@ -39,39 +72,41 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: new AppBar(title: new Text("Guia Bixo"),),
         body: Center(
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-          Container(
-              height: 570,
-              decoration: BoxDecoration(color: const Color(0xff7c94b6)),
-              child: getListView()
-          ),
-
-          Container(
-            height: 40,
-            child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                      controller: userInput,
-                    ),
+                  Container(
+                      height: 570,
+                      decoration: BoxDecoration(color: const Color(0xff7c94b6)),
+                      child: new ListView.builder(itemBuilder: _buildTileItem, itemCount: tiles.length,)
                   ),
-                  Expanded(
-                      child: FlatButton(
-                    color: Colors.blue,
-                    splashColor: Colors.blueAccent,
-                    child: Text(
-                      "Enviar",
-                    ),
-                    onPressed: () {
-                      getNewMessage(userInput.text);
-                    },
-                  ))
-                ]),
-          ),
-        ])));
+
+                  Container(
+                    height: 40,
+                    child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          Expanded(
+                            child: TextField(
+                              controller: userInput,
+                            ),
+                          ),
+                          Expanded(
+                              child: FlatButton(
+                                color: Colors.blue,
+                                splashColor: Colors.blueAccent,
+                                child: Text(
+                                  "Enviar",
+                                ),
+                                onPressed: () {
+                                  setListTiles(userInput.text);
+                                  (context as Element).markNeedsBuild();
+                                },
+                              ))
+                        ]),
+                  ),
+                ])));
   }
 }
